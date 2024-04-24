8bda85a10c93bfb40b4f8e2cdaf57014dae6da77
Tibor Blascsok <tiborblascsok02@gmail.com>
2024-04-24 13:35:49 +0200

    Currently selected screen looks diff

--- MoodTracker/src/Menu.java
+++ MoodTracker/src/Menu.java
@@ -1,8 +1,5 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,9 +13,9 @@ public class Menu {
         panel.setBackground(new Color(236, 203, 124));
         panel.setBounds(0,760,414,100);
 
-        JButton calendarButton = getButtons().get(0);
-        JButton otherButton = getButtons().get(1);
-        JButton otherSecondButton = getButtons().get(2);
+        JButton calendarButton = getButtons(currentScreen).get(0);
+        JButton otherButton = getButtons(currentScreen).get(1);
+        JButton otherSecondButton = getButtons(currentScreen).get(2);
 
         panel.add(calendarButton);
         panel.add(otherButton);
@@ -28,14 +25,15 @@ public class Menu {
         return panel;
     }
 
-    private List<JButton> getButtons() throws IOException {
+    private List<JButton> getButtons(String currentScreen) throws IOException {
         //https://www.flaticon.com/free-icons/calendar Calendar icons created by Freepik - Flaticon
         JButton calendarButton = new JButton(new ImageIcon("images/calendarIcon.png"));
         calendarButton.setFocusPainted(false);
         calendarButton.setVisible(true);
         calendarButton.setOpaque(false);
+        calendarButton.setBorderPainted(true);
         calendarButton.setBackground(new Color(236, 203, 124));
-        calendarButton.setBorderPainted(false);
+        calendarButton.setBorder(BorderFactory.createLineBorder(new Color(236, 203, 124)));
         calendarButton.setBounds(20, 10, 80, 80);
 
         //<a href="https://www.flaticon.com/free-icons/analysis" title="analysis icons">Analysis icons created by deemakdaksina - Flaticon</a>
@@ -43,18 +41,25 @@ public class Menu {
         otherButton.setFocusPainted(false);
         otherButton.setVisible(true);
         otherButton.setOpaque(false);
-        otherButton.setBackground(new Color(236, 203, 124));
         otherButton.setBorderPainted(false);
+        otherButton.setBackground(new Color(236, 203, 124));
+        otherButton.setBorder(BorderFactory.createLineBorder(new Color(236, 203, 124)));
         otherButton.setBounds(160, 10, 80, 80);
 
         JButton otherSecondButton = new JButton(new ImageIcon("images/settingIcon.png"));
         otherSecondButton.setFocusPainted(false);
         otherSecondButton.setVisible(true);
         otherSecondButton.setOpaque(false);
-        otherSecondButton.setBackground(new Color(236, 203, 124));
         otherSecondButton.setBorderPainted(false);
+        otherSecondButton.setBackground(new Color(236, 203, 124));
+        otherSecondButton.setBorder(BorderFactory.createLineBorder(new Color(236, 203, 124)));
         otherSecondButton.setBounds(300, 10, 80, 80);
 
+        switch(currentScreen) {
+            case "calendar": calendarButton.setBorder(BorderFactory.createLineBorder(new Color(101, 85, 32), 3));; break;
+            case "analytics": otherButton.setBorder(BorderFactory.createLineBorder(new Color(101, 85, 32), 3));; break;
+            case "settings": otherSecondButton.setBorder(BorderFactory.createLineBorder(new Color(101, 85, 32), 3));; break;
+        }
 
         List<JButton> buttons =  new ArrayList<>(Arrays.asList(
                 calendarButton, otherButton, otherSecondButton));
