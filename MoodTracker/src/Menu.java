d47b790db9ae2d3f383bb4dfa3133b02df9df28b
Tibor Blascsok <tiborblascsok02@gmail.com>
2024-04-28 12:53:59 +0200

    menu refactor

--- MoodTracker/src/Menu.java
+++ MoodTracker/src/Menu.java
@@ -1,33 +1,24 @@
 import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Menu {
+    private final JButton calendarButton = new JButton(new ImageIcon("images/calendarIcon.png"));
+    private final JButton otherButton = new JButton(new ImageIcon("images/analyticsIcon.png"));
+    private final JButton otherSecondButton = new JButton(new ImageIcon("images/settingIcon.png"));
+    private final JPanel panel = new JPanel();
+
     public JPanel navigationMenu(String currentScreen) throws IOException {
-        JPanel panel = new JPanel();
+        setButtons(currentScreen);
         panel.setLayout(null);
         panel.setVisible(true);
         panel.setBackground(new Colors().secondaryBackgroundColor);
         panel.setBounds(0,760,414,100);
 
-        JButton calendarButton = getButtons(currentScreen).get(0);
-        JButton otherButton = getButtons(currentScreen).get(1);
-        JButton otherSecondButton = getButtons(currentScreen).get(2);
-
-        panel.add(calendarButton);
-        panel.add(otherButton);
-        panel.add(otherSecondButton);
-
-
         return panel;
     }
 
-    private List<JButton> getButtons(String currentScreen) throws IOException {
+    private void setButtons(String currentScreen) throws IOException {
         //https://www.flaticon.com/free-icons/calendar Calendar icons created by Freepik - Flaticon
-        JButton calendarButton = new JButton(new ImageIcon("images/calendarIcon.png"));
         calendarButton.setFocusPainted(false);
         calendarButton.setVisible(true);
         calendarButton.setOpaque(false);
@@ -36,8 +27,7 @@ public class Menu {
         calendarButton.setBorder(BorderFactory.createLineBorder(new Colors().secondaryBackgroundColor));
         calendarButton.setBounds(20, 10, 80, 80);
 
-        //<a href="https://www.flaticon.com/free-icons/analysis" title="analysis icons">Analysis icons created by deemakdaksina - Flaticon</a>
-        JButton otherButton = new JButton(new ImageIcon("images/analyticsIcon.png"));
+        //https://www.flaticon.com/free-icons/analysis Analysis icons created by deemakdaksina - Flaticon</a>
         otherButton.setFocusPainted(false);
         otherButton.setVisible(true);
         otherButton.setOpaque(false);
@@ -46,7 +36,6 @@ public class Menu {
         otherButton.setBorder(BorderFactory.createLineBorder(new Colors().secondaryBackgroundColor));
         otherButton.setBounds(160, 10, 80, 80);
 
-        JButton otherSecondButton = new JButton(new ImageIcon("images/settingIcon.png"));
         otherSecondButton.setFocusPainted(false);
         otherSecondButton.setVisible(true);
         otherSecondButton.setOpaque(false);
@@ -56,16 +45,13 @@ public class Menu {
         otherSecondButton.setBounds(300, 10, 80, 80);
 
         switch(currentScreen) {
-            case "calendar": calendarButton.setBorder(BorderFactory.createLineBorder(new Color(101, 85, 32), 3));; break;
-            case "analytics": otherButton.setBorder(BorderFactory.createLineBorder(new Color(101, 85, 32), 3));; break;
-            case "settings": otherSecondButton.setBorder(BorderFactory.createLineBorder(new Color(101, 85, 32), 3));; break;
+            case "calendar": calendarButton.setBorder(BorderFactory.createLineBorder(new Colors().borderColor, 3));; break;
+            case "analytics": otherButton.setBorder(BorderFactory.createLineBorder(new Colors().borderColor, 3));; break;
+            case "settings": otherSecondButton.setBorder(BorderFactory.createLineBorder(new Colors().borderColor, 3));; break;
         }
 
-        List<JButton> buttons =  new ArrayList<>(Arrays.asList(
-                calendarButton, otherButton, otherSecondButton));
-        return buttons;
-
-
-
+        panel.add(calendarButton);
+        panel.add(otherButton);
+        panel.add(otherSecondButton);
     }
 }
